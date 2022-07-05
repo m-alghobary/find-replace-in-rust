0ccc92b28782e002c03c1c1fda15e9be54e6db9e
m-alghobary <m.alghobary90@gmail.com>
2022-07-05 21:10:36 +0300

    basic replace functionality done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use std::env::{self};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = env::args();
     let params = replace::Params::new(args);
     params.validate();
 
-    println!("{:?}", params);
+    let in_file = std::fs::read_to_string(&params.input_file)?;
+
+    let mut result = Vec::<String>::new();
+    for line in in_file.lines() {
+        if line.contains(&params.term) {
+            result.push(line.replace(&params.term, "foo"));
+        }
+    }
+
+    let result = result.join("\n");
+
+    println!("{}", result);
+
+    std::fs::write(&params.input_file, result)?;
+
+    println!("Replace done");
+
+    Ok(())
 }
